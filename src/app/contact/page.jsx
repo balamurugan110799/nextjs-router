513dd14db820e04a886c3c54d4997c49276f4810
balamurugan110799 <balamurugan99711@gmail.com>
2024-04-19 11:28:10 +0530

    updates0

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,7 +1,16 @@
 
+import Head from 'next/head';
 
 export default function contact() {
   return (
-    <div>contact</div>
+    <div>
+           <Head>
+        {/* Add meta tags */}
+        <title>Conatct US</title> {/* Specify the title of the page */}
+        <meta name="description" content="Conatct US" /> {/* Add meta description */}
+        <meta name="keywords" content="Conatct US" /> {/* Add meta keywords */}
+        {/* Add other meta tags as needed */}
+      </Head>
+        contact</div>
   )
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,18 @@
 import Image from "next/image";
+import Head from 'next/head';
+
 // import Im from "../../public/assets/"
 export default function Home() {
   return (
     <main>
+            <Head>
+        {/* Add meta tags */}
+        <title>My Page Title</title> {/* Specify the title of the page */}
+        <meta name="description" content="Description of my page for SEO" /> {/* Add meta description */}
+        <meta name="keywords" content="keyword1, keyword2, keyword3" /> {/* Add meta keywords */}
+        {/* Add other meta tags as needed */}
+      </Head>
+
       Welcome World
       <Image
         src="/assets/react.png" // Specify the path to your image
