b2bc820676ce2428f489566dd2cc1d89b7ac0f6d
balamurugan110799 <balamurugan99711@gmail.com>
2024-04-19 09:35:50 +0530

    updates0

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-    domains: ['https://balamurugan110799.github.io/nextjs-router'],
-    unoptimized: true,
-    },
-    distDir:"dist",
-    
- // Add basePath
-  basePath: '/nextjs-router',
-
-};
+const nextConfig = {};
 
 export default nextConfig;
